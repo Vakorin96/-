1452d61e875a01c995b16740cf601b973445e177
ЧУПАКАБРА <artem.vakorin.96@mail.ru>
2023-04-13 11:43:10 +0700

    искать определенный элемент массива

---
+++ 1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 11, 12, 13, 14, 15, 61, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
